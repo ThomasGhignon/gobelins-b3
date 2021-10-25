ad44ba3538f80cb195435610bda2eba440dcdd76
ThomasGhignon <ghignon.thomas@gmail.com>
2021-10-25 14:09:38 +0200

    6.1 *ngFor in PricingComponent

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricingComponent implements OnInit {
 
-  constructor() { }
+  pricingData: any[];
+
+  constructor() {
+    this.pricingData = [
+      {
+        price: '29$',
+        bandwidth: '1GB',
+        space: '50MB',
+        support: 'No',
+        domain: 1
+      },
+      {
+        price: '49$',
+        bandwidth: '5GB',
+        space: '80MB',
+        support: 'Yes',
+        domain: 4
+      },
+      {
+        price: '79$',
+        bandwidth: '10GB',
+        space: '120MB',
+        support: 'Yes',
+        domain: 8
+      }
+    ];
+  }
 
   ngOnInit(): void {
   }
