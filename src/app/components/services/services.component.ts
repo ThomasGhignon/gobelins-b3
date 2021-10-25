e8f577b7fa6069763ff0b62f60da2edc39f65148
ThomasGhignon <ghignon.thomas@gmail.com>
2021-10-25 14:50:39 +0200

    6.4 *ngFor and ngClass in ServicesComponent

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -7,7 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  servicesData: any[];
+
+  constructor() {
+    this.servicesData = [
+      {
+        img: 'ti-settings',
+        title: 'Digital Design',
+        content: 'Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        img: 'ti-palette',
+        title: 'Unlimited Colors',
+        content: 'Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        img: 'ti-stats-up',
+        title: 'Strategy Solutions',
+        content: 'Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        img: 'ti-package',
+        title: 'Awesome Support',
+        content: 'Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        img: 'ti-dashboard',
+        title: 'Truly Multipurpose',
+        content: 'Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        img: 'ti-headphone',
+        title: 'Easy to customize',
+        content: 'Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+    ];
+  }
 
   ngOnInit(): void {
   }
