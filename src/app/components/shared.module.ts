e6512eecfcf3d18c8e8a52a5bdcfc12c1a618c46
ThomasGhignon <ghignon.thomas@gmail.com>
2021-10-25 11:38:05 +0200

    4.1 création component footer

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -7,12 +7,14 @@ import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
 import {ClientComponent} from './client/client.component';
 import {SwitcherComponent} from './switcher/switcher.component';
 import {ScrollspyDirective} from './scrollspy.directive';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     ClientComponent,
     SwitcherComponent,
-    ScrollspyDirective
+    ScrollspyDirective,
+    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -22,7 +24,8 @@ import {ScrollspyDirective} from './scrollspy.directive';
   exports: [
     ClientComponent,
     SwitcherComponent,
-    ScrollspyDirective
+    ScrollspyDirective,
+    FooterComponent
   ]
 })
 export class SharedModule {
