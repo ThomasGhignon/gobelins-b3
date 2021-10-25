630124f563074c0e6b3e6160ab13f586744ebb2e
ThomasGhignon <ghignon.thomas@gmail.com>
2021-10-25 14:31:17 +0200

    6.2 ngClass in PricingComponent

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -16,24 +16,36 @@ export class PricingComponent implements OnInit {
         bandwidth: '1GB',
         space: '50MB',
         support: 'No',
-        domain: 1
+        domain: 1,
+        isActive: true
       },
       {
         price: '49$',
         bandwidth: '5GB',
         space: '80MB',
         support: 'Yes',
-        domain: 4
+        domain: 4,
+        isActive: true
       },
       {
         price: '79$',
         bandwidth: '10GB',
         space: '120MB',
         support: 'Yes',
-        domain: 8
+        domain: 8,
+        isActive: false
       }
     ];
   }
+
+  /*getClass(item:any): string{
+    if (item.isActive === true){
+      return 'active';
+    }else {
+      return '';
+    }
+
+  }*/
 
   ngOnInit(): void {
   }
