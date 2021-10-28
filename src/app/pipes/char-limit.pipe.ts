c23f095afad851516c53d974c8649ed501048a5d
ThomasGhignon <ghignon.thomas@gmail.com>
2021-10-28 08:54:57 +0200

    11.1.2 pipes

--- src/app/pipes/char-limit.pipe.ts
+++ src/app/pipes/char-limit.pipe.ts
@@ -6,9 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CharLimitPipe implements PipeTransform {
 
   transform(value: string, limit :number): any {
-    value = value.substring(0, limit) + "...";
-
-    return value;
+    if (value){
+      return (value.length > limit) ? value.substring(0, limit) + "..." : value;
+    }
+    return '';
   }
 
 }
